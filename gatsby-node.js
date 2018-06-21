0282eea423642d1ca468057a534ee7460165d094
Oleg Obidin <oleg.obidin@revolut.com>
2018-06-21 10:54:36 +0300

    gatsby-node.js: migrate to Gatsby v2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,26 @@
-exports.modifyWebpackConfig = ({ config }, options = {}) => {
-  config.loader('url-loader', {
-    test: /\.(jpg|jpeg|png|gif|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
-    loader: 'url',
-    query: {
-      limit: 10000,
-      name: 'static/[name].[hash:8].[ext]',
-    },
+exports.onCreateWebpackConfig = ({ actions, loaders }, options = {}) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.(jpg|jpeg|png|gif|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
+          use: [
+            loaders.url({
+              limit: 10000,
+              name: 'static/[name].[hash:8].[ext]',
+            }),
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: require.resolve('svg-sprite-loader'),
+              options,
+            },
+          ],
+        },
+      ]
+    }
   });
-
-  config.loader('svg-sprite', {
-    loader: 'svg-sprite-loader',
-    test: /\.svg$/,
-    query: options,
-  });
-
-  return config;
 };
